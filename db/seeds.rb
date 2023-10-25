431cf11878c0cca25b8e0973fd37902beedc41f1
allyssoncomin <allysson_luan@hotmail.com>
2023-10-25 12:43:47 -0300

    Rails Yeld done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+puts "creating restaurants"
+25.times do
+  Restaurant.create(
+    {
+      name: Faker::Restaurant.name,
+      address: Faker::Address.full_address,
+      phone_number: Faker::PhoneNumber.cell_phone,
+      category: Restaurant::CATEGORY.sample
+    }
+  )
+end
+puts "restaurantes created !"
